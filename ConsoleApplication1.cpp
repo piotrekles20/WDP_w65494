1b96525277d83b55dd173e7ea275126ed8f7d5f8
piotrekles20 <88188402+piotrekles20@users.noreply.github.com>
2021-12-10 17:41:44 +0100

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,213 @@
+﻿// ConsoleApplication1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//deklaracja zmiennych
+
+	//int a;
+	//float a1, b, c = 12;
+	//char znak = 's';
+	//string imie = "jan";
+
+	//cout << "Witaj, "<< imie <<"!"<< endl;
+	//cout << "Podaj liczbe: " , cin >> a;
+	//cout << "Podaj kolejna liczbe: " << endl;
+	//	cin >> a1;
+
+
+
+
+
+
+	//int a = 22;
+
+	//if (a >= 0)
+	//{
+	//	cout << "Liczba jest dodatnia" << endl;
+	//}
+	//else
+	//{
+	//	cout << "Liczba jest ujemna" << endl;
+	//}
+
+
+
+
+
+	//int x;
+	//cout << "Wprowadz liczbe: " << endl;
+	//cin >> x;
+	//if (x % 2) {
+	//	cout << "Liczba jest nieparzysta" << endl;
+	//}
+	//else {
+	//	cout << "Liczba jest parzysta" << endl;
+	//}
+
+
+
+
+//	float a, b;
+//	cout << "Wprowadz dwie liczby: " , cin >> a >> b;
+//	if (a > b) {
+//			cout << "Liczba " << a << " jest wieksza" << endl;
+//		}
+//		if (b > a) {
+//			cout << "Liczba " << b << " jest wieksza" << endl;
+//
+//			}
+
+
+
+
+//	float a, b, c, d, srednia;
+//	cout << "Wprowadz cztery liczby: ", cin >> a >> b >> c >> d;
+//
+//	srednia = (a + b + c + d) / 4;
+//	cout << "Srednia: " << srednia << endl;
+//	cout << "Srednia: " << (a + b + c + d) / 4 << endl;
+
+
+
+
+/*int wybor;
+	float a, b;
+
+	cout << "Podaj a:" , cin >> a;
+	cout << "Podaj b:" , cin >> b;
+
+	cout << "Operacje matematyczne: \n1.Suma \n2.Roznica \n3.Iloczyn \n4.Iloraz \n5.Twoj wybor:";
+	cin >> wybor;
+
+	switch (wybor)
+	{
+	case 1:
+		cout << "Suma " << a << " + " << b << " = " << a + b << endl;
+		break;
+
+	case 2:
+		cout << "Roznica " << a << " - " << b << " = " << a - b << endl;
+		break;
+	case 3:
+		cout << "Iloczyn " << a << " * " << b << " = " << a * b << endl;
+		break;
+	case 4:
+		if (b != 0)
+			cout << "Iloraz " << a << " / " << b << " = " << a / b << endl;
+		else cout << "Nie mozna dzielic przez 0!\n";
+		break;
+	default:
+		cout << "Wybrano nieistniejaca operacje\n";
+		break;
+
+	}*/
+
+
+
+	/*	int i = 0;
+		cout << "Petla While:" << endl;
+		while (i < 5)
+		{
+			cout << i << " ";
+			i++;
+		}
+
+		cout << "\nPetla Do While:" << endl;
+
+		i = 0;
+		do {
+			cout << i << " ";
+			i++;
+		} while (i < 5);
+
+
+		cout << "\nFor:" << endl;
+
+
+		for (int i = 0; i < 5; i++)
+		{
+			cout << i << " ";
+		}*/
+
+
+
+
+		/*
+		{
+			int n, n1;
+			float lpPkt=0, suma = 0;
+
+			cout << "Podaj liczbe studentow: ", cin >> n;
+			n1 = n;
+
+			if (n > 0)
+			{
+				while (n > 0)
+				{
+					cout << "Podaj liczbe pkt", cin >> lpPkt;
+
+					if (lpPkt >= 0 && lpPkt <= 100)
+					{	suma += lpPkt;
+						n--;
+					}
+					if (n == 0) break;
+					continue;
+				}
+				cout << "Srednia ilosc pkt wynosi: " << suma / n1 << endl;
+			}
+
+			else cout << "W grupie musi byc conajmniej jeden student" << endl;
+		}
+
+		*/
+
+
+
+
+/*
+	int a, sd = 0, su = 0, iu = 0, id = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "Podaj liczbe ", cin >> a;
+		if (a = 0)
+		{
+			sd += a;//sd=sd+a
+			id++;
+		}
+		else {
+			su += a;
+			iu++;
+		}
+	}
+	cout << "Suma dodatnia " << sd << " Ilosc liczb dodatnich " << id << endl;
+	cout << "Suma ujemna " << su << " Ilosc liczb ujemnych " << iu << endl;
+	*/
+
+
+
+/*
+int n, liczba, suma=0;
+
+cout << "Podaj n: ", cin >> n;
+
+if (n > 0) {
+
+	for (int i = 0; i < n; i++) {
+		cout << "Podaj liczbe: ", cin >> liczba;
+		if (liczba % 2 == 0) suma += liczba;
+	}
+	cout << "Suma liczb parzystych: " << suma << endl;
+}
+else cout << "N nie moze byc liczba ujemna";
+}
+*/
+
+
+
+
+
